1d2efcd9d3c6553114638138305651894e3f759c
saymon shoab <mdsaymonshoab@gmail.com>
2021-11-09 00:15:07 +0600

    tailwind added

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import React from "react";
 
 function App() {
   return (
-    <div>
+    <div className="text-red-400 text-center font-serif text-5xl">
       <h1>Hello World</h1>
       <p>Ei beta ki koros</p>
+      <h1 className="">Hello World</h1>
+      <h1>Hello World</h1>
       <h1>Hello World</h1>
       <h1>Hello World</h1>
       <h1>Hello World</h1>
