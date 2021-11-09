7402f5736e30c050c1667a38a036d9a0c17d7465
shazim8480 <troublmaker8480@gmail.com>
2021-11-09 20:59:08 +0600

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,9 @@ function App() {
   return (
     <div>
       <h1>Hello World</h1>
-<<<<<<< HEAD
       <p>Ei beta ki koros</p>
-=======
       <h1>Hello World</h1>
       <h1>Hello World</h1>
->>>>>>> develop
     </div>
   );
 }
