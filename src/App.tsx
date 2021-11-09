ea14be22f3392e2b01c1fd00246267e24a18d92e
saymon shoab <mdsaymonshoab@gmail.com>
2021-11-09 20:45:12 +0600

    vul

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React from "react";
+import {Navigate, Route, Routes} from "react-router-dom"
 
 function App() {
   return (
-    <div className="text-red-400 text-center font-serif text-5xl">
-      <h1>Hello World</h1>
-      <p>Ei beta ki koros</p>
-      <h1 className="">Hello World</h1>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
-    </div>
+      <>
+        <div className="">
+           <Routes>
+              <Route  />
+
+             
+           </Routes>
+        </div>
+      </> 
   );
 }
 
